f7725a2dae72becd9cefc2f32f8f95a8e04a46b0
Dan Esparza <dan.esparza@teachingstrategies.com>
2023-12-08 15:20:40 -0500

    Initial commit

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/danesparza/genqclient_test
+
+go 1.21.0
+
+require (
+	github.com/Khan/genqlient v0.6.0 // indirect
+	github.com/agnivade/levenshtein v1.1.1 // indirect
+	github.com/alexflint/go-arg v1.4.2 // indirect
+	github.com/alexflint/go-scalar v1.0.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.31.0 // indirect
+	github.com/vektah/gqlparser/v2 v2.5.1 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/tools v0.8.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"github.com/Khan/genqlient/graphql"
+	"github.com/rs/zerolog/log"
+	"net/http"
+	"os"
+)
+
+type authedTransport struct {
+	key     string
+	wrapped http.RoundTripper
+}
+
+func (t *authedTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	req.Header.Set("Authorization", "bearer "+t.key)
+	return t.wrapped.RoundTrip(req)
+}
+
+func main() {
+	log.Info().Msg("Starting up...")
+
+	key := os.Getenv("GITHUB_TOKEN")
+	if key == "" {
+		log.Error().Msg("must set GITHUB_TOKEN=<github token>")
+		return
+	}
+
+	//	Create the http client with the token
+	httpClient := http.Client{
+		Transport: &authedTransport{
+			key:     key,
+			wrapped: http.DefaultTransport,
+		},
+	}
+
+	//	Create the context and the graphQL client
+	ctx := context.Background()
+	graphqlClient := graphql.NewClient("https://api.github.com/graphql", &httpClient)
+
+	//	Call the function:
+	resp, err := getUser(ctx, graphqlClient, "danesparza")
+	if err != nil {
+		log.Err(err).Msg("problem calling getUser")
+		return
+	}
+
+	log.Info().Str("Name", resp.User.Name).Msg("Success!")
+}
+
+//go:generate go run github.com/Khan/genqlient genqlient.yaml
